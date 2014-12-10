0751b822dc1ad5b8b39c1716d630bad8c05432f7
John Brzustowski <jbrzusto at fastmail dot fm>
2014-12-10 14:51:24 +0000

    grab appropriate location/orientation data from geo table

--- R/getSweep.R
+++ R/getSweep.R
@@ -28,6 +28,7 @@
 #' (when bit 8 is 0), each sample occupies a whole number of bytes.
 #' \item ns, number of samples per pulse
 #' \item scale, maximum possible value of sample.
+#' \item heading, angle of heading pulse (azimuth=0) in degrees clockwise from North
 #' }
 #' 
 #' @note Requires that library RSQLite already be loaded.
@@ -43,7 +44,11 @@ getSweep = function(ts, map) {
     swp = dbGetQuery(con, sprintf("select sweep_key from pulses where ts >= %f order by ts limit 1", ts))[1, 1]
     rv = dbGetQuery(con, sprintf("select ts,trigs,trig_clock,azi,elev,rot,samples from pulses where sweep_key = %d order by ts", swp))
     mode = dbGetQuery(con, sprintf("select mode_key from pulses where sweep_key=%d order by ts limit 1", swp))[1, 1]
-    params = dbGetQuery(con, sprintf("select t2.rate, t2.format, t2.ns, t2.scale from modes as t1 join digitize_modes as t2 on t1.digitize_mode_key = t2.digitize_mode_key where t1.mode_key=%d", mode))
+    params = unclass(dbGetQuery(con, sprintf("select t2.rate, t2.format, t2.ns, t2.scale from modes as t1 join digitize_modes as t2 on t1.digitize_mode_key = t2.digitize_mode_key where t1.mode_key=%d", mode)))
+    ## get latest (but preceding) position / orientation value from geo table.
+    ## FIXME: Interpolate.  (doesn't matter for fixed site).
+    
+    params = c(params, unclass(dbGetQuery(con, sprintf("select lat, lon, alt, heading from geo where ts <= %f order by ts desc limit 1", ts))))
     dbDisconnect(con)
     for (p in names(params))
         attr(rv, p) = params[[p]]
