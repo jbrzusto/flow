0e8f88ab35ff2f9bf062d7fea3a96d16d62cc35c
john <jbrzusto@fastmail.fm>
2015-03-02 14:08:57 +0000

    cache SQLITE connections by default; open .sqlite files read-only

--- R/getSweep.R
+++ R/getSweep.R
@@ -7,6 +7,15 @@
 #' @param ts: scalar, numeric timestamp (unclassed POSIXct)
 #' 
 #' @param map: function mapping timestamps to SQLite files, as returned by \code{scanFolder}
+#'
+#' @param persist: logical; if \code{FALSE}, each call to \code{getSweep}
+#' opens and closes an sqlite connection to the appropriate database
+#' file.  Otherwise, the default, the function maintains a
+#' connection to the database for the current sweep, storing it in
+#' variable \code{con}, and its name in \code{dbfile}, in
+#' \code{environment(map)}.  Subsequent calls to \code{getSweep} will use the
+#' existing connection if appropriate, otherwise, will close it and replace
+#' it with a new one.
 #' 
 #' @return NULL, if \code{map} does not include \code{ts}, otherwise a data.frame with
 #' these columns:
@@ -35,24 +44,58 @@
 #' 
 #' @author John Brzustowski \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
 
-getSweep = function(ts, map) {
+getSweep = function(ts, map, persist = TRUE) {
     ts = as.numeric(ts)
     f = map(ts)
     if (is.na(f))
         return(NULL)
-    con = dbConnect("SQLite", f)
+    env = environment(map)
+    con = NULL
+
+    ## check for an existing connection and re-use or close as appropriate
+    if (!is.null(env$dbfile)) {
+        if (persist && f == env$dbfile) {
+            con = env$con
+        } else {
+            ## close connection, which is to different file
+            dbDisconnect(env$con)
+            env$con = NULL
+            env$dbfile = NULL
+        }
+    }
+
+    ## open a new connection if we didn't re-use a persistent one
+    if (is.null(con)) {
+        con = dbConnect("SQLite", f, flags=1L) ## open read-only
+        if (persist) {
+            ## retain for later use
+            env$con = con
+            env$dbfile = f
+        }
+    }
+
+    ## find the appropriate sweep in this file
     swp = dbGetQuery(con, sprintf("select sweep_key from pulses where ts >= %.3f order by ts limit 1", ts))[1, 1]
+
+    ## get all pulses from this sweep
     rv = dbGetQuery(con, sprintf("select ts,trigs,trig_clock,azi,elev,rot,samples from pulses where sweep_key = %d order by ts", swp))
+
+    ## get metadata for this sweep
     mode = dbGetQuery(con, sprintf("select mode_key from pulses where sweep_key=%d order by ts limit 1", swp))[1, 1]
     params = unclass(dbGetQuery(con, sprintf("select t2.rate, t2.format, t2.ns, t2.scale from modes as t1 join digitize_modes as t2 on t1.digitize_mode_key = t2.digitize_mode_key where t1.mode_key=%d", mode)))
+    
     ## get latest (but preceding) position / orientation value from geo table.
     ## FIXME: Interpolate.  (doesn't matter for fixed site).
     
     params = c(params, unclass(dbGetQuery(con, sprintf("select lat, lon, alt, heading from geo where ts <= %f order by ts desc limit 1", ts))))
-    dbDisconnect(con)
     params$res = 2.99792458E8 / (2 * params$rate)    ## input range cell size, in metres
 
     attr(rv, "radar.meta") = params
     class(rv) = c(class(rv), "Sweep")
+
+    ## close the connection, if not persisting
+    if (!persist)
+        dbDisconnect(con)
+
     return(rv)
 }
